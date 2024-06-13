75a9ef60b8c98146ccacc199fd60ec2a6242e52c
Johann six <six.johann@gmail.com>
2024-06-13 09:35:24 +0200

    🎨 update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,45 +5,28 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import HeaderPage from '$lib/components/HeaderPage.svelte';
+	import SidebarPage from '$lib/components/SidebarPage.svelte';
+	import Copyright from '$lib/components/Copyright.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<HeaderPage />
 	</svelte:fragment>
 	<!-- Page Route Content -->
+	<svelte:fragment slot="sidebarLeft">
+		<SidebarPage />
+	</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
 	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">
+		<Copyright />
+	</svelte:fragment>
+	<!-- (footer) -->
 </AppShell>
