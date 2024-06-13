ecf8fd987929aefbd6dd6a699f09b718edee3fd3
Johann six <six.johann@gmail.com>
2024-06-13 14:25:20 +0200

    🐛 fix with sveltekit 2.0

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,14 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
 import type { AuthSystemFields, TypedPocketBase, UsersRecord } from '$lib/models/pocketbase-types';
-
-declare namespace App {
-	interface Locals {
-		pb: TypedPocketBase;
-		user: AuthSystemFields<UsersRecord> | undefined;
+import type { AuthModel } from 'pocketbase';
+import type PocketBase from 'pocketbase';
+export declare global {
+	namespace App {
+		// interface Error {}
+		interface Locals {
+			// pb: TypedPocketBase;
+			pb: PocketBase;
+			user: AuthModel | undefined;
+		}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
 	}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
 }
+
