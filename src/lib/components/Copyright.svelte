1799e460eaceb518bb4ecf9c00337af9f7bf7bcc
Johann six <six.johann@gmail.com>
2024-06-13 09:34:01 +0200

    🎨 copyright

---
+++ src/lib/components/Copyright.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const year = new Date().getFullYear();
+</script>
+
+<div class="w-full p-1">
+	<p class="text-tertiary-700-200-token text-nowrap text-center">
+		Copyright Handy Team 2024 - {year}
+	</p>
+</div>
+
+<style lang="postcss">
+	p {
+		line-height: 1;
+		font-size: 0.5rem;
+	}
+</style>
