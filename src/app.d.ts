46950254ace86af6d39e6e9f058e3297628fca2b
Johann six <six.johann@gmail.com>
2024-06-13 09:31:55 +0200

    ✨ add pocketbase

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+import type { AuthSystemFields, TypedPocketBase, UsersRecord } from '$lib/models/pocketbase-types';
+
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		pb: TypedPocketBase;
+		user: AuthSystemFields<UsersRecord> | undefined;
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
---
+++ src/hooks.server.ts
@@ -0,0 +1,29 @@
+import PocketBase from 'pocketbase';
+
+import type { Handle } from '@sveltejs/kit';
+import { env } from '$env/dynamic/public';
+import { serializeNonPOJOs } from '$lib/utils/serializeNonPojos';
+import type { AuthSystemFields, TypedPocketBase, UsersRecord } from '$lib/models/pocketbase-types';
+
+
+export const handle: Handle = async ({ event, resolve }) => {
+	event.locals.pb = new PocketBase(env.PUBLIC_API_URL) as TypedPocketBase;
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+	try {
+		if (event.locals.pb.authStore.isValid) {
+			await event.locals.pb.collection('users').authRefresh();
+			// Add type user
+			event.locals.user = serializeNonPOJOs<AuthSystemFields<UsersRecord>>(event.locals.pb.authStore.model);
+		}
+	} catch (_) {
+		event.locals.pb.authStore.clear();
+		event.locals.user = undefined;
+	}
+
+	const response = await resolve(event);
+
+	response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false,  }));
+
+	return response;
+};
