b5b8a15001286db40df17c44bcff7b38d3f043a6
Johann six <six.johann@gmail.com>
2024-06-15 14:03:25 +0200

    ✨ create card basic component

---
+++ src/lib/components/CardsBasic.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+export let title: string;
+
+</script>
+<div class="card">
+    <header class="card-header">
+        <h2 class="text-xl font-semibold text-primary-500 text-center">{title}</h2>
+    </header>
+    <section class="p-4">
+       <slot />
+    </section>
+    <slot name=footer />
+</div>
