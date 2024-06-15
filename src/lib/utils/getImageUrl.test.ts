6e6adec5dc1f24e8d31eb031aa04a1ca31363e9b
Johann six <six.johann@gmail.com>
2024-06-15 15:33:39 +0200

    ✨ Add getImageURL utility function and tests

---
+++ src/lib/utils/getImageUrl.test.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest';
+import { getImageURL } from "./getImageUrl";
+
+
+describe("getImageURL", () => {
+    it("should return the correct image URL", () => {
+        const collectionId = "collection123";
+        const recordId = "record456";
+        const fileName = "image.jpg";
+        const size = "200x200";
+
+        const expectedURL = `${process.env.PUBLIC_IMAGE_URL}/${collectionId}/${recordId}/${fileName}?size=${size}`;
+
+        const result = getImageURL(collectionId, recordId, fileName, size);
+
+        expect(result).toBe(expectedURL);
+    });
+
+    it("should return the default image URL when size is not provided", () => {
+        const collectionId = "collection123";
+        const recordId = "record456";
+        const fileName = "image.jpg";
+
+        const expectedURL = `${process.env.PUBLIC_IMAGE_URL}/${collectionId}/${recordId}/${fileName}?size=0x0`;
+
+        const result = getImageURL(collectionId, recordId, fileName);
+
+        expect(result).toBe(expectedURL);
+    });
+});
---
+++ src/lib/utils/getImageUrl.ts
@@ -0,0 +1,6 @@
+import { env } from "$env/dynamic/public";
+
+export const getImageURL = (collectionId: string, recordId: string, fileName: string, size = '0x0') => {
+    const baseUrl = env.PUBLIC_API_URL;
+	return `${baseUrl}api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
+};
