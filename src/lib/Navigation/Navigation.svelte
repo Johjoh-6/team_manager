94e6c5f1db0ab07e97de7342fb9a39df24d69ae3
Johann six <six.johann@gmail.com>
2024-06-15 17:26:05 +0200

    ♻️ refactor navigation component to improve accessibility and styling

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
 
 	const drawerStore = getDrawerStore();
 
@@ -9,10 +10,10 @@
 </script>
 
 <nav class="list-nav flex-grow p-4">
-	<ul>
-		<li><a href="/" on:click={drawerClose}>Accueil</a></li>
-		<li><a href="/calendar" on:click={drawerClose}>Calendrier</a></li>
-		<li><a href="/players" on:click={drawerClose}>Nos joueurs</a></li>
-		<li><a href="/history" on:click={drawerClose}>Historique de match</a></li>
+	<ul class="flex flex-col gap-1"> 
+		<li><a class="btn hover:variant-ghost-primary text-xl font-semibold p-2 {$page.url.pathname === '/' ? 'bg-primary-active-token' : 'variant-ghost-surface'}" href="/" on:click={drawerClose} aria-current={$page.url.pathname === '/' ? 'page' : undefined}>Accueil</a></li>
+		<li><a class="btn hover:variant-ghost-primary text-xl font-semibold p-2 {$page.url.pathname === '/calendar' ? 'bg-primary-active-token' : 'variant-ghost-surface'}" href="/calendar" on:click={drawerClose} aria-current={$page.url.pathname === '/calendar' ? 'page' : undefined} data-sveltekit-preload-data="hover">Calendrier</a></li>
+		<li><a class="btn hover:variant-ghost-primary text-xl font-semibold p-2 {$page.url.pathname === '/players' ? 'bg-primary-active-token' : 'variant-ghost-surface'}" href="/players" on:click={drawerClose} aria-current={$page.url.pathname === '/players' ? 'page' : undefined} data-sveltekit-preload-data="hover">Nos joueurs</a></li>
+		<li><a class="btn hover:variant-ghost-primary text-xl font-semibold p-2 {$page.url.pathname === '/history' ? 'bg-primary-active-token' : 'variant-ghost-surface'}" href="/history" on:click={drawerClose} aria-current={$page.url.pathname === '/history' ? 'page' : undefined} data-sveltekit-preload-data="hover">Historique de match</a></li>
 	</ul>
 </nav>
