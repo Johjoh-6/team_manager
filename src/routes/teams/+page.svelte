b8040a3f255fe48ff2ceb43e9125a0c552d1e2df
Johann six <six.johann@gmail.com>
2024-06-16 16:48:58 +0200

    💄 change font for team name and hover on button

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -56,9 +56,9 @@ const handleReset = async(): Promise<void> =>{
     
     <p>Trouver son équipe</p>
     <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-        <div class="input-group-shim"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-token w-4 h-4"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path></svg></div>
+        <div class="input-group-shim"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-token w-5 h-5"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path></svg></div>
         <input type="search" placeholder="équipe ..." bind:value={search} />
-        <button class="variant-filled-secondary" on:click={handleSearch}>Trouver</button>
+        <button class="variant-filled-secondary hover:variant-ghost-secondary" on:click={handleSearch}>Trouver</button>
         </div>
 		
         {#if data.teams.length === 0}
@@ -73,7 +73,7 @@ const handleReset = async(): Promise<void> =>{
                     <Avatar width='w-12 md:w-16' src={team.logo != '' ? getImageURL(team.collectionId, team.id, team.logo, '24x24') : ''} alt={team.name} />
                 </svelte:fragment>
                 <svelte:fragment slot="summary">
-                    <p>{team.name}</p>
+                    <p class="text-lg font-semibold text-primary-500">{team.name}</p>
                 </svelte:fragment>
                 <svelte:fragment slot="content">
                     <div class="flex flex-col gap-1">
