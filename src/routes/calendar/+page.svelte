e441c7830847363b2c02ae8c838cabf694cf7827
Johann six <six.johann@gmail.com>
2024-06-22 12:45:07 +0200

    🎨 update search bar for responsive

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -70,7 +70,12 @@
 		<button
 			class="variant-filled-secondary hover:variant-ghost-secondary"
 			on:click={handleSearch}
-			on:keydown={handleKeySearch}>Trouver</button
+			on:keydown={handleKeySearch}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-white h-5 w-5 md:hidden"><path d="M3 12.9999H9V10.9999H3V1.84558C3 1.56944 3.22386 1.34558 3.5 1.34558C3.58425 1.34558 3.66714 1.36687 3.74096 1.40747L22.2034 11.5618C22.4454 11.6949 22.5337 11.9989 22.4006 12.2409C22.3549 12.324 22.2865 12.3924 22.2034 12.4381L3.74096 22.5924C3.499 22.7255 3.19497 22.6372 3.06189 22.3953C3.02129 22.3214 3 22.2386 3 22.1543V12.9999Z"></path></svg>
+			<span class="hidden md:block">
+				Trouver
+			</span>
+			</button
 		>
 	</div>
 
@@ -85,15 +90,15 @@
 			{#each data.events as event}
 				<a
 					href="/calendar/{event.id}"
-					class="card card-hover grid grid-cols-2 grid-rows-2 gap-2 p-2 md:grid-cols-1 md:grid-rows-none"
+					class="card card-hover flex flex-col gap-2 p-2 "
 				>
-					<p class="col-span-2 text-center text-lg capitalize md:col-span-1">
+					<p class=" text-center text-lg capitalize ">
 						{event.name ?? 'Non renseigné'}
 					</p>
 					<span class="badge bg-primary-500 p-2 text-white"
 						>{event?.expand?.type.name ?? 'Non défini'}</span
 					>
-					<p class="content-center text-nowrap md:text-center">
+					<p class="content-center text-nowrap text-center">
 						{dateFormatFr(event.date_start)} / {dateFormatFr(event.date_end)}
 					</p>
 				</a>
