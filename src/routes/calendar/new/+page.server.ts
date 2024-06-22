7d1436f5111a5085c39d4a40ea93c14ee323fe78
Johann six <six.johann@gmail.com>
2024-06-22 13:07:51 +0200

    ✨ add page create new calendar

---
+++ src/routes/calendar/new/+page.server.ts
@@ -0,0 +1,62 @@
+import { message, superValidate } from 'sveltekit-superforms';
+import type { PageServerLoad } from './$types';
+import { zod } from 'sveltekit-superforms/adapters';
+import { eventSchema } from '$lib/models/schemaEvent';
+import { error, redirect } from '@sveltejs/kit';
+import isRole from '$lib/utils/isRole';
+import { Roles } from '$lib/enum/rolesEnum';
+import { ClientResponseError } from 'pocketbase';
+
+export const load = (async ({ locals }) => {
+	try {
+		if (!isRole(Roles.MANAGER, locals.user?.expand.role)) {
+			throw new Error('Forbidden');
+		}
+
+		const eventType = await locals.pb.collection('event_type').getFullList();
+
+		
+		const form = await superValidate(zod(eventSchema));
+
+		return {
+			eventType,
+			form
+		};
+	} catch (err) {
+		console.error(err);
+		redirect(303, '/calendar');
+	}
+}) satisfies PageServerLoad;
+
+export const actions = {
+	default: async ({ locals, request }) => {
+		const form = await superValidate(request, zod(eventSchema));
+		console.log('form', form);
+		if (!form.valid) {
+			return { form };
+		}
+		
+		  try {
+			// transform the date to the right format
+			form.data.date_start = new Date(form.data.date_start).toISOString();
+			form.data.date_end = new Date(form.data.date_end).toISOString();
+            
+			// create the event data in your database
+			await locals.pb.collection('events').create(form.data);
+			return message(form, '');
+		  } catch (err) {
+			console.error('Error: ', err);
+			if (err instanceof ClientResponseError) {
+				if (err.status === 400) {
+					return message(form, "Erreur lors de la création", {
+						status: 400
+					});
+				}
+			}
+			error(500, {
+				message: "Quelque chose s'est mal passé lors de la connexion. Veuillez réessayer plus tard."
+			});
+		}
+		return { form };
+	},
+};
---
+++ src/routes/calendar/new/+page.svelte
@@ -0,0 +1,143 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+	import type { PageData } from './$types';
+	import { getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
+  
+	export let data: PageData;
+  
+	const toastStore = getToastStore();
+  
+	// Client API:
+	const { form, errors, enhance, message } = superForm(data.form, {
+		resetForm: false,
+		applyAction: false,
+		onResult({ result }) {
+			if (result.status === 200) {
+				const t: ToastSettings = {
+					message: 'Création avec succès',
+					background: 'bg-success-500',
+					timeout: 3000,
+                    callback: () => {
+                        goto('/calendar')
+                    }
+				};
+				toastStore.trigger(t);
+				return;
+			}
+		}
+	});
+
+	$: if ($message) {
+		const t: ToastSettings = {
+			message: $message,
+			background: 'bg-error-500',
+			timeout: 3000
+		};
+		toastStore.trigger(t);
+	}
+  </script>
+  
+  <svelte:head>
+	<title>Crée l'événement</title>
+  </svelte:head>
+  
+  <section class="content-grid content-grid justify-center p-4">
+	<div
+		class="bg-surface-300-600-token flex w-full flex-col gap-4 p-8 shadow-md rounded-container-token"
+	>
+		<h1 class="text-3xl font-semibold text-primary-500 text-center">Crée un évenement</h1>
+		<form
+			class="form grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4"
+			use:enhance
+			method="POST"
+		>
+        <input type="text" hidden value={data.team?.id} id="team" name="team">
+			<label class="label" for="name">
+				<span>Nom de l'évenement</span>
+				<input
+					class="input {$errors.name && 'input-error'}"
+					type="text"
+					id="name"
+					name="name"
+					bind:value={$form.name}
+				/>
+				{#if $errors.name}
+					<p class="text-error-500">{$errors.name}</p>
+				{/if}
+			</label>
+			<label class="label" for="type">
+				<span>Type d'évenement</span>
+				<select class="select capitalize" name="type" id="type" bind:value={$form.type}>
+					<option value="">Choisir un type d'évenement</option>
+					{#each data.eventType as t}
+					<option value={t.id} selected={$form.type == t.id}>
+					  {t.name}
+					</option>
+				  {/each}
+				</select>
+				{#if $errors.type}
+					<p class="text-error-500">{$errors.type}</p>
+				{/if}
+			</label>
+
+			<label class="label col-span-2" for="location">
+				<span>Location</span>
+				<input
+					class="input {$errors.location && 'input-error'}"
+					type="text"
+					id="location"
+					name="location"
+					bind:value={$form.location}
+				/>
+				{#if $errors.location}
+					<p class="text-error-500">{$errors.location}</p>
+				{/if}
+			</label>
+			<label class="label" for="date-start">
+				<span>Date de début</span>
+				<input
+				  class="input {$errors.date_start && 'input-error'}"
+				  type="datetime-local"
+				  id="date-start"
+				  name="date_start"
+				  bind:value={$form.date_start}
+				/>
+				{#if $errors.date_start}
+				  <p class="text-error-500">{$errors.date_start}</p>
+				{/if}
+			  </label>
+			  
+			  <label class="label" for="date-end">
+				<span>Date de fin</span>
+				<input
+				  class="input {$errors.date_end && 'input-error'}"
+				  type="datetime-local"
+				  id="date-end"
+				  name="date_end"
+				  bind:value={$form.date_end}
+				/>
+				{#if $errors.date_end}
+				  <p class="text-error-500">{$errors.date_end}</p>
+				{/if}
+			  </label>
+
+			<label class="label md:col-span-2">
+				<span>Notes </span><span class="text-token text-xs opacity-85"> (facultatif)</span>
+				<textarea
+					class="textarea {$errors.note && 'input-error'}"
+					rows="4"
+					placeholder="Notes sur l'évenement."
+					id="note"
+					name="note"
+					bind:value={$form.note}
+				/>
+				{#if $errors.note}
+					<p class="text-error-500">{$errors.note}</p>
+				{/if}
+			</label>
+			
+	  <button type="submit" class="variant-filled-primary btn mt-2 md:col-span-2">Créer</button>
+	</form>
+  </section>
+  
