b4154806d214ea3419927efd89ce7cc65049adec
Johann six <six.johann@gmail.com>
2024-06-23 16:54:36 +0200

    🥅 handle error is player is null

--- src/routes/teams/[id]/+page.svelte
+++ src/routes/teams/[id]/+page.svelte
@@ -15,8 +15,10 @@
 </svelte:head>
 
 <section class="content-grid flow mt-4">
-	<CardsBasic >
-		<h1 slot="header"  class="text-xl p-2 text-center text-primary-500 font-semibold">{team.name}</h1>
+	<CardsBasic>
+		<h1 slot="header" class="p-2 text-center text-xl font-semibold text-primary-500">
+			{team.name}
+		</h1>
 		{#if team.logo}
 			<img
 				src={getImageURL(team.collectionId, team.id, team.logo, '24x24')}
@@ -33,58 +35,60 @@
 		{/if}
 	</CardsBasic>
 	<CardsBasic>
-		<h2 slot="header" class="text-primary-500 font-semibold text-center text-lg">Team info</h2>
-		<div class="grid md:grid-cols-2 gap-2">
-			<p class="font-semibold text-primary-500 text-center">Localisation</p>
+		<h2 slot="header" class="text-center text-lg font-semibold text-primary-500">Team info</h2>
+		<div class="grid gap-2 md:grid-cols-2">
+			<p class="text-center font-semibold text-primary-500">Localisation</p>
 			<p class="capitalize">{team.location}</p>
-			<p class="font-semibold text-primary-500 text-center">Description</p>
+			<p class="text-center font-semibold text-primary-500">Description</p>
 			<p>{team.description}</p>
-			<p class="font-semibold text-primary-500 text-center">Contact</p>
+			<p class="text-center font-semibold text-primary-500">Contact</p>
 			<p>{team.contact_info}</p>
-			<p class="font-semibold text-primary-500 text-center">Manager</p>
-			<p class="capitalize text-center">{managerName !== '' ? managerName : 'Non renseigné'}</p>
+			<p class="text-center font-semibold text-primary-500">Manager</p>
+			<p class="text-center capitalize">{managerName !== '' ? managerName : 'Non renseigné'}</p>
 		</div>
 	</CardsBasic>
 	{#if team?.expand?.players}
-	<CardsBasic urlToGo="/players">
-		<h2 slot="header" class="text-primary-500 font-semibold text-center text-lg">Nombre de joueurs<span>({team?.expand?.players?.length ?? 0})</span></h2>
-		{#if team?.expand?.players && team?.expand?.players?.length === 0}
-			<p class="text-center">Aucun joueur dans l'équipe</p>
-		{:else}
-			<ul>
-				{#each team?.expand?.players as player}
-					<li class="flex w-full flex-nowrap items-center justify-between gap-2">
-						<Avatar
-							src={player.picture != ''
-								? getImageURL(player.collectionId, player.id, player.picture, '24x24')
-								: '/avatar.jpg'}
-							fallback="/avatar.jpg"
-							alt="Photos du joueur"
-							class="avatar"
-						/>
-						<p class="p-2">
-							<strong class="capitalize">
-								{player.last_name ?? 'Non renseigné'}
-							</strong>
-							{' '}
-							{player.first_name ?? 'Non renseigné'}
-						</p>
-						<p>{player.expand.position.name}</p>
-						<p class="text-xl font-semibold">
-							#{player.player_number ?? '?'}
-						</p>
-					</li>
-				{/each}
-			</ul>
-		{/if}
-	</CardsBasic>
+		<CardsBasic urlToGo="/players">
+			<h2 slot="header" class="text-center text-lg font-semibold text-primary-500">
+				Nombre de joueurs<span>({team?.expand?.players?.length ?? 0})</span>
+			</h2>
+			{#if team?.expand?.players && team?.expand?.players?.length === 0}
+				<p class="text-center">Aucun joueur dans l'équipe</p>
+			{:else}
+				<ul>
+					{#each team?.expand?.players as player}
+						<li class="flex w-full flex-nowrap items-center justify-between gap-2">
+							<Avatar
+								src={player.picture != ''
+									? getImageURL(player.collectionId, player.id, player.picture, '24x24')
+									: '/avatar.jpg'}
+								fallback="/avatar.jpg"
+								alt="Photos du joueur"
+								class="avatar"
+							/>
+							<p class="p-2">
+								<strong class="capitalize">
+									{player.last_name ?? 'Non renseigné'}
+								</strong>
+								{' '}
+								{player.first_name ?? 'Non renseigné'}
+							</p>
+							<p>{player.expand.position.name}</p>
+							<p class="text-xl font-semibold">
+								#{player.player_number ?? '?'}
+							</p>
+						</li>
+					{/each}
+				</ul>
+			{/if}
+		</CardsBasic>
 	{/if}
 	{#if !data.team}
-								<button
-									class="btn bg-primary-500 font-bold text-white hover:text-token hover:ring-primary-400-500-token hover:bg-transparent"
-									on:click={() => {
-										console.log('join team', team.id);
-									}}>Rejoindre</button
-								>
-							{/if}
+		<button
+			class="btn bg-primary-500 font-bold text-white hover:text-token hover:ring-primary-400-500-token hover:bg-transparent"
+			on:click={() => {
+				console.log('join team', team.id);
+			}}>Rejoindre</button
+		>
+	{/if}
 </section>
