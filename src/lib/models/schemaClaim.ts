b7c5e583a992250331869c17d72fc4ddca235069
Johann six <six.johann@gmail.com>
2024-07-01 09:25:09 +0200

    🎨 add missing field

--- src/lib/models/schemaClaim.ts
+++ src/lib/models/schemaClaim.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 
 export const claimSchema = z.object({
     id: z.string().optional(),
+    teamID: z.string().optional(),
     userID: z.string().optional(),
     playerID: z.string().optional(),
     status: z.enum(['pending', 'approved', 'rejected'])
