3b367fe939bdf44ea2eaa4edf8df9c8e25e91e6b
Johann six <six.johann@gmail.com>
2024-07-01 16:04:13 +0200

    🎨 add missing field for picture

--- src/routes/teams/[id]/+page.server.ts
+++ src/routes/teams/[id]/+page.server.ts
@@ -8,7 +8,7 @@ export const load = (async ({ params, locals }) => {
 			throw new Error('Id is missing');
 		}
 		const teamId = await locals.pb.collection('teams').getOne(id, {
-			fields: '*,expand.manager.last_name,expand.manager.first_name,expand.sport.name,expand.players.last_name,expand.players.picture,expand.players.first_name,expand.players.player_number,expand.players.expand.position.name',
+			fields: '*,expand.manager.last_name,expand.manager.first_name,expand.sport.name,expand.players.last_name,expand.players.picture,expand.players.collectionId,expand.players.id,expand.players.first_name,expand.players.player_number,expand.players.expand.position.name',
 			expand: 'sport,manager,players.position'
 		});
 		return { teamId };
