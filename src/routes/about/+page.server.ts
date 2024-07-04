9efd1fa73b3b65adf26b86dd4dfc9902bc638cdf
Johann six <six.johann@gmail.com>
2024-07-04 18:21:07 +0200

    🎨 add content for roadmap

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -1,5 +1,15 @@
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
-    return {};
+export const load = (async ({locals}) => {
+    try {
+		const roadmap = await locals.pb.collection('roadmap').getFullList();
+
+		return {
+			roadmap: roadmap
+		};
+    } catch (err) {
+        // console.error(err);
+        error(500, err?.message ?? 'Erreur inconnue');
+    }
 }) satisfies PageServerLoad;
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,4 +2,55 @@
     import type { PageData } from './$types';
     
     export let data: PageData;
+
+    const statusColor = (status: string)=> {
+        switch(status){
+            case 'available':
+                return 'variant-filled-success';
+            case 'progress':
+                return 'variant-filled-secondary';
+            case 'pending':
+                return 'variant-filled-warning';
+            default:
+                return 'variant-filled';
+        }
+    
+    }
+    const translateStatus = (status: string)=> {
+        switch(status){
+            case 'available':
+                return 'Disponible';
+            case 'progress':
+                return 'En cours';
+            case 'pending':
+                return 'A venir';
+            default:
+                return 'Inconnu';
+        }
+    
+    }
 </script>
+
+<section class="content-grid mt-4 flow">
+    <div class="full-width grid place-content-center">
+        <h1>Roadmap</h1>
+        <p class="text-center">C'est ici que nous vous informerons sur le futur de l'application</p>
+    </div>
+    {#each data.roadmap as r}
+        
+        <div class="flex flex-col gap-2">
+            <h2>{r.title} <span class="badge {statusColor(r.status)}">{translateStatus(r.status)}</span></h2>
+            {@html r.description}
+            </div>
+    {/each}
+    
+</section>
+
+<style lang="postcss">
+    h1{
+        @apply text-3xl text-primary-500 text-center font-bold p-4;
+    }
+    h2{
+        @apply text-xl text-primary-500 font-bold text-center p-4;
+    }
+</style>
