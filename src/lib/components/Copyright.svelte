0e48241cfb64340fa0596f67c5ba1bb979308863
Johann six <six.johann@gmail.com>
2024-07-04 17:43:58 +0200

    ✨ add rgpd

--- src/lib/components/Copyright.svelte
+++ src/lib/components/Copyright.svelte
@@ -2,15 +2,22 @@
 	const year = new Date().getFullYear();
 </script>
 
-<div class="w-full p-1">
+<div class="w-full p-1 flex gap-2 justify-center">
 	<p class="text-tertiary-700-200-token text-nowrap text-center">
 		Copyright Handy Team 2024 - {year}
 	</p>
+	<a href="/rgpd" class="text-tertiary-700-200-token text-nowrap text-center cursor-pointer hover:text-primary-500">
+		Informations Légales & Politique de confidentialité
+	</a>
 </div>
 
 <style lang="postcss">
 	p {
-		line-height: 1;
-		font-size: 0.5rem;
+		line-height: 1.1;
+		font-size: 0.6rem;
+	}
+	a {
+		font-size: 0.6rem;
+		line-height: 1.1;
 	}
 </style>
---
+++ src/routes/rgpd/+page.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+    const owner = "Johann Six";
+    const emailOwner = "support@johann-six.fr";
+    const url = "https://team.colify.johann-six.fr/";
+</script>
+
+<section class="content-grid flow">
+
+
+<h1>RGPD</h1>
+<h2>1 - Édition du site</h2>
+
+<p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet {url} l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:</p>
+
+<p>Propriétaire du site : <strong>{owner}</strong> - Contact : <a href="mailto:{emailOwner}">{emailOwner}</a> 0761000882 - Adresse : 27120.</p>
+
+<p>Identification de l'entreprise : <strong>{owner}</strong> au capital social de € - SIREN : - RCS ou RM : - Adresse postale : 27120 - [Consignes : ajoutez ici le lien hypertexte vers la page de vos conditions générales de vente si vous en avez une]</p>
+
+<p>Directeur de la publication : <strong>{owner}</strong> - Contact : <a href="mailto:{emailOwner}">{emailOwner}</a>.</p>
+
+<p>Hébergeur : AutreHetzner Hetzner Online GmbH Industriestr. 25 91710 Gunzenhausen, Deutschland Tel.: +49 (0)9831 505-0 E-Mail: info@hetzner.com</p>
+
+<p>Délégué à la protection des données : <strong>{owner}</strong> -</p>
+
+<p>Autres contributeurs :</p>
+<h2>
+2 - Propriété intellectuelle et contrefaçons.
+</h2>
+<p><strong>{owner}</strong> est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons.</p>
+
+<p>Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de <strong>{owner}</strong>.</p>
+
+<p>Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+
+<h2>
+3 - Limitations de responsabilité.
+</h2>
+<p><strong>{owner}</strong> ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site {url}.</p>
+
+<p><strong>{owner}</strong> décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur {url}.</p>
+
+<p><strong>{owner}</strong> s’engage à sécuriser au mieux le site {url}, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.</p>
+
+<p>Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. <strong>{owner}</strong> se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données.</p>
+
+<p>Le cas échéant, <strong>{owner}</strong> se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).</p>
+
+<h2>
+4 - CNIL et gestion des données personnelles.
+</h2>
+<p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site {url} dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message à notre Délégué à la Protection des Données : <strong>{owner}</strong> - .</p>
+
+<p>Pour plus d'informations sur la façon dont nous traitons vos données (type de données, finalité, destinataire...), lisez notre {url}rgpd. [Consignes : ajoutez ici le lien hypertexte vers votre politique de confidentialité]</p>
+
+<h2>
+5 - Liens hypertextes et cookies
+</h2>
+<p>Le site {url} contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers {url}.</p>
+
+<p>La navigation sur le site {url} est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.</p>
+
+<p>Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.</p>
+
+<p>Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement.</p>
+
+<p>Les cookies sont enregistrés pour une durée maximale de 1 mois.</p>
+
+<p>Pour plus d'informations sur la façon dont nous faisons usage des cookies, lisez notre {url}rgpd. [Consignes : ajoutez ici le lien hypertexte vers votre politique de confidentialité ou vers votre politique de cookies si vous en avez une (c'est le cas si vous utilisez Complianz)]</p>
+
+<h2>
+6 - Droit applicable et attribution de juridiction.
+</h2>
+<p>Tout litige en relation avec l’utilisation du site {url} est soumis au droit français. En dehors des cas où la loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Rouen.</p>
+</section>
+
+<style lang="postcss">
+    h1{
+        @apply text-2xl font-bold text-primary-500 text-center;
+    
+    }
+    h2{
+        @apply text-lg font-semibold text-primary-500;
+    }
+    p{
+        @apply text-base text-tertiary-700-200-token;
+    }
+    a{
+        @apply text-primary-500;
+    }
+
+</style>
