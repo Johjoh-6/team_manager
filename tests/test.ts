f1946920c2e612a3281c8f9b77ae04ccbf8cf6e8
Johann six <six.johann@gmail.com>
2024-07-16 13:57:37 +0200

    🧪 add test integration

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,8 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Bienvenue' })).toBeVisible();
 });
+
+test('expect to have the copyrigth text', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText('Copyright Teams Handi 2024 -')).toBeVisible();
+});
